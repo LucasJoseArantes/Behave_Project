aaeeb61ee1600d51f02187a37df219ee568beba1
Rafael Prado <lucas.js@estudante.iftm.edu.br>
2024-03-20 21:33:26 -0300

    Update GET ADRESS request

--- features/steps/api/api.py
+++ features/steps/api/api.py
@@ -1,11 +1,17 @@
 from features.helpers.api_helpers import _make_request
+import logging as log
+
 
 class ApiInterface:
     def make_get_adress_request(context, url):
         return _make_request(url, context , "GET")
     
     def check_response_status_code(self, response, expected_status_code):
+        log.info(f"Response status code: {response}")
         assert response == expected_status_code, f"Expected status code {expected_status_code}, but got {response}"
 
-    def check_adress_response (response_json_adress, expected_adress)
+    def check_adress_response(self, response_json_adress, expected_adress):
+        log.info(f"Response json: {response_json_adress}")
+        assert response_json_adress == expected_adress, f"Expected adress {expected_adress}, but got {response_json_adress}"
+        
     
--- features/steps/api/api_steps.py
+++ features/steps/api/api_steps.py
@@ -2,19 +2,28 @@ from behave import given, when, then
 
 from features.common.constants import info
 
+
 from features.steps.api.api import ApiInterface
 
 @given("I make a GET request using '{}' url")
 def step_make_get_request( context, get_url):
     api_interface = ApiInterface()
+    # context.response = api_interface.make_get_adress_request(info["url_requests"][get_url])
     context.response = api_interface.make_get_adress_request(info["url_requests"][get_url])
 
 @then("I should receive a successful response")
 def step_check_receive_successful_response(context):
     api_interface = ApiInterface()
     api_interface.check_response_status_code(context.response.status_code, 200)
+    
 
 @then("I check the adress response")
 def step_check_adress_response(context):
     api_interface = ApiInterface()
-    api_interface.check_adress_response(context.response.json(), info["adress_data"])
+    response_json = context.response.json()
+    filter_adress = {
+        'logradouro': response_json['logradouro'],
+        'bairro': response_json['bairro'],
+        'localidade': response_json['localidade']
+    }
+    api_interface.check_adress_response(filter_adress, info["adress_data"])
